acbf42a6ee39b8986c93d86a2a339c0a02fbce66
Carol-88 <gralleta@gmail.com>
2023-08-29 19:46:07 +0200

    preparacion para deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-require('dotenv').config();
 const cors = require("cors");
 const path = require("path");
 
